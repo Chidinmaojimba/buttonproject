0ebdd514ade0849f229b4e516c73ab830a8d6f0c
chidinma <chidinmaojimba888@gmail.com>
2025-06-04 16:58:46 -0700

    update reset button

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 
 function buttonItems(selector, itemdivSelector){
-const but = document.querySelector(selector);
-const itemdiv = document.querySelector(itemdivSelector);
+    const but = document.querySelector(selector);
+    const itemdiv = document.querySelector(itemdivSelector);
 
 
-let count = 0;
+    let count = 0;
 
-but.addEventListener('click', () =>{
+    but.addEventListener('click', () =>{
 
- //let count = 0;
- count++   
- let insideText = but.innerText;
- let num =  "<p>" +  count + "</p>";
- itemdiv.innerHTML += num;
+        //let count = 0;
+        count++   
+        let insideText = but.innerText;
+        let num =  "<p>" +  count + "</p>";
+        itemdiv.innerHTML = num;
 
-resetbut.addEventListener("click", () =>{
-
-
-})
- 
-
-}
-)
-        
+    })       
 }
 
 buttonItems(".but1", ".itemdiv1")
@@ -32,13 +24,12 @@ buttonItems(".but4", ".itemdiv4")
 
 
 
-const resetBut = document.querySelector(".reset");
+const resetBut = document.querySelector("#reset");
 const items = document.querySelectorAll(".item");
 resetBut.addEventListener("click", () =>{
-    for (const item of items){
+    for (const item of items){  
         item.innerHTML = "";
     }
-
 
 })
  
